a72f8163209e44d01eacdbd4a96f231be45420b1
Brent Abrahams <brent_abrahams@yahoo.com>
2015-07-17 14:25:13 +0200

    Updated dependencies

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'babrahams:temple',
-  version: '0.1.7',
+  version: '0.2.0',
   summary: 'Developer tool that provides visual information about templates',
   git: 'https://github.com/JackAdams/temple.git',
   documentation: 'README.md',
@@ -15,7 +15,7 @@ Package.onUse(function(api) {
   api.use('reactive-dict', 'client');
   
   api.use('gwendall:body-events@0.1.6', 'client');
-  api.use('constellation:console@1.0.7', {weak: true}),
+  api.use('constellation:console@1.1.0', {weak: true}),
   
   api.addFiles('temple.css','client');
   api.addFiles('temple.html', 'client');
